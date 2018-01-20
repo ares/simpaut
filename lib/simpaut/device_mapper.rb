b129dd4a6a4714ab87a1cc8cf74993b68c28b57d
Marek Hulan <mhulan@redhat.com>
2018-01-20 10:06:25 +0100

    Add global configuration support

--- lib/simpaut/device_mapper.rb
+++ lib/simpaut/device_mapper.rb
@@ -1,8 +1,11 @@
 require 'yaml'
 
 class DeviceMapper
+  GLOBAL_CONFIG_PATH = '/etc/simpaut/settings.yml'
+
   def self.find_by_name(name, logger)
-    @devices ||= YAML.load_file('config/settings.yml')['devices']
+    path = File.exists?(GLOBAL_CONFIG_PATH) ? GLOBAL_CONFIG_PATH : 'config/settings.yml'
+    @devices ||= YAML.load_file(path)['devices']
     attrs = @devices[name.to_s]
     attrs[:logger] = logger
     SamsungWamApi::Device.new(attrs.symbolize_keys)
