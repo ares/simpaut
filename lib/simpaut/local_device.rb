07a0e1be492c79c5e01b0876f1ea312598500b57
Marek Hulan <mhulan@redhat.com>
2021-01-05 20:52:57 +0100

    Add power status support for local devices

--- lib/simpaut/local_device.rb
+++ lib/simpaut/local_device.rb
@@ -2,10 +2,11 @@ require 'shellwords'
 require 'simpaut/device'
 
 class LocalDevice < Device
-  def initialize(on_command:, off_command:, env: '', **args)
+  def initialize(on_command:, off_command:, status_command: nil, env: '', **args)
     super(**args)
     @on_command = on_command
     @off_command = off_command
+    @status_command = status_command
     @env = env
   end
 
@@ -17,6 +18,27 @@ class LocalDevice < Device
     command!(@off_command)
   end
 
+  def on?
+    power_status == 1
+  end
+
+  def off?
+    power_status == 0
+  end
+
+  def power_status
+    return 'unsupported' if @status_command.nil?
+
+    command(@status_command) ? 1 : 0 
+  end
+
+  def command(cmd)
+    command = "#{@env} #{cmd}"
+    @logger.debug "executing shell: #{command}"
+    `#{command}`
+    $?.success?
+  end
+
   def command!(cmd)
     command = "#{@env} #{cmd}"
     @logger.debug "executing shell: #{command}"
@@ -26,5 +48,6 @@ class LocalDevice < Device
     else
       @logger.error "execution failed, stdout follows\n#{stdout}"
     end
+    $?.success?
   end
 end
